59d75f427f0b4fded49a50a32098a5d99ef6cc1b
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-07-07 15:26:14 +0200

    Mise en place des routes

---
+++ src/components/APropos.js
@@ -0,0 +1,5 @@
+export const APropos = () => {
+  return (
+    <div>A Propos Page</div>
+  )
+}
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+export const PageNotFound = () => {
+  return (
+    <div>
+        <p>404 Page</p>
+    </div>
+  )
+}
