478240541187150d15d108ae71e21dcb49ec1bf1
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-07-08 10:55:22 +0200

    Mise en place de la structure des dossiers & fichiers

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Home } from './components/Home';
 import { Accueil } from './components/Accueil';
 import { APropos } from './components/APropos';
 import { PageNotFound } from './components/PageNotFound';
+import { Navbar } from './components/header/Navbar'
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='Accueil' element={<Accueil />} />
       <Route path='APropos' element={<APropos />} />
       <Route path='*' element={<PageNotFound />} />
     </Routes>
+    </>
   );
 }
 
